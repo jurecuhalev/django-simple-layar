af65359d3968b05f53661af37f1c87b71accb448
Jure Cuhalev <gandalfar@gmail.com>
2011-07-03 23:23:11 +0200

    lets make static work on gondor

--- invaders/settings.py
+++ invaders/settings.py
@@ -49,7 +49,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = "/site_media/static/"
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
