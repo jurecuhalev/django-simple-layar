c506b53e5fd58ce73f46738e1203142d43e50695
Jure Cuhalev <gandalfar@gmail.com>
2011-10-31 19:00:06 +0100

    making ljubljana invaders into more generic app

--- invaders/web/models.py
+++ invaders/web/models.py
@@ -1,7 +1,6 @@
 from django.contrib.gis.db import models
 
-# Create your models here.
-class InvaderLocation(models.Model):
+class Location(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     location = models.PointField()
--- invaders/web/views.py
+++ invaders/web/views.py
@@ -1,16 +1,16 @@
 from django.contrib.gis.geos import Point
-from web.models import InvaderLocation
+from web.models import Location
 from layar import LayarView, POI
 from django.contrib.gis.measure import D
 
 default_unit = 'm'
 class InvaderLayar(LayarView):
     # make sure to accept **kwargs
-    def get_ljubljanainvaders_queryset(self, latitude, longitude, radius, **kwargs):
-        print InvaderLocation.objects.filter(location__distance_lte=(Point(longitude, latitude), D(m=radius)))
-        return InvaderLocation.objects.filter(location__distance_lte=(Point(longitude, latitude), D(m=radius)))
+    def get_mfru_queryset(self, latitude, longitude, radius, **kwargs):
+        print Location.objects.filter(location__distance_lte=(Point(longitude, latitude), D(m=radius)))
+        return Location.objects.filter(location__distance_lte=(Point(longitude, latitude), D(m=radius)))
 
-    def poi_from_ljubljanainvaders_item(self, item):
+    def poi_from_mfru_item(self, item):
         return POI(id=item.id, lat=item.location.y, lon=item.location.x, title=item.name,
                     line2=item.description, line3='Distance: %distance%', image_url='http://np794.o1.gondor.io'+item.image_url)
 
