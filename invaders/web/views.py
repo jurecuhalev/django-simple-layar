971ec1bb3d855ee2b364c384f1ac1ad92583eb4a
Jure Cuhalev <gandalfar@gmail.com>
2011-11-26 11:09:35 +0100

    lets try to send a custom CIW

--- invaders/web/views.py
+++ invaders/web/views.py
@@ -11,15 +11,16 @@ HOST = settings.HOST
 class InvaderLayar(LayarView):
     items = [1.0, 1.2, 1.5, 2.0, 2.3, 2.5, 2.8, 3.0, 3.3, 3.5, 3.8, 4.0]
 
-    # make sure to accept **kwargs
     def get_mfru_queryset(self, latitude, longitude, radius, **kwargs):
+    # def get_mfrudev_queryset(self, latitude, longitude, radius, **kwargs):
         return Location.objects.filter(location__distance_lte=(Point(longitude, latitude), D(m=radius)))
 
     def poi_from_mfru_item(self, item):
+    # def poi_from_mfrudev_item(self, item):
         relative_alt = round(random.uniform(1, 6), 1)
 
         return POI(id=item.id, lat=item.location.y, lon=item.location.x, relative_alt=random.choice(self.items),
-                    title=item.name,
+                    title=item.name, type=1,
                     line2=item.description, line3='Distance: %distance%', 
                     image_url=HOST+item.image.url, actions=[{'label':'Opis dogodka', 'uri':item.url}])
 
